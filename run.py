70a6fa40cd0842f7c59648f38d9003e30638d8c6
Ubuntu <ubuntu@ip-172-31-18-52.ec2.internal>
2019-09-24 20:50:43 +0000

    Final Commit

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ def about_recipe(name):
 if __name__=="__main__":
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False) 
+            debug=True) 
             
 
 
